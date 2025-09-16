a2aab6760502951e4b5e8b7d62621cc948b5faa6
Eduardoooo0 <el957733@gmail.com>
2025-09-16 20:37:08 -0300

    adicionando modelo sqlmodel

--- backend/models.py
+++ backend/models.py
@@ -1,2 +1,15 @@
-from pydantic import BaseModel
+from sqlmodel import SQLModel, Field, Relationship
 
+class Area_Conhecimento(SQLModel, table=True):
+    id: int = Field(primary_key=True)
+    nome: str = Field(index=False)
+    tipo: str = Field(index=False)
+    questoes: list["Questao"] = Relationship(back_populates="area_conhecimento")
+
+class Questao(SQLModel, table=True):
+    id: int = Field(primary_key=True)
+    descricao: str = Field(index=False)
+    dificuldade: str = Field(index=False)
+    assunto: str = Field(index=False)
+    area_conhecimento_id: int = Field(foreign_key="area_conhecimento.id")
+    area_conhecimento: "Area_Conhecimento" = Relationship(back_populates="questoes")
