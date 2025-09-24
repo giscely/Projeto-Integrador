793fbf3bea767347b38a920be22f51c885a353e5
Eduardo Lucas de Medeiros Silva <20221101110056@ifrn.local>
2025-09-24 15:54:52 -0300

    modificando database

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, Request
+from sqlmodel import Session
+from database import get_session, create_db_and_tables
+from typing import Annotated
+from contextlib import asynccontextmanager
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
-app = FastAPI()
 
-@app.get('/')
-def index():
-    return 'index page'
+SessionDep = Annotated[Session, Depends(get_session)]
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+# templates = Jinja2Templates(directory="templates")
+
+# @app.get("/", response_class=HTMLResponse)
+# async def index(request: Request):
+#     return templates.TemplateResponse("index.html", {"request": request, "nome": "Usuário"})
--- backend/models.py
+++ backend/models.py
@@ -1,15 +1,22 @@
 from sqlmodel import SQLModel, Field, Relationship
 
+
+class Usuario(SQLModel, table=True):
+    usu_id: int =  Field(default=None,primary_key=True)
+    usu_nome: str = Field(index=False)
+    usu_email: str = Field(index=False, unique=True)
+    usu_senha: str = Field(index=False)
+
 class Area_Conhecimento(SQLModel, table=True):
-    id: int = Field(primary_key=True)
-    nome: str = Field(index=False)
-    tipo: str = Field(index=False)
+    acc_id: int = Field(default=None,primary_key=True)
+    acc_nome: str = Field(index=False)
+    acc_tipo: str = Field(index=False)
     questoes: list["Questao"] = Relationship(back_populates="area_conhecimento")
 
 class Questao(SQLModel, table=True):
-    id: int = Field(primary_key=True)
-    descricao: str = Field(index=False)
-    dificuldade: str = Field(index=False)
-    assunto: str = Field(index=False)
-    area_conhecimento_id: int = Field(foreign_key="area_conhecimento.id")
+    qst_id: int = Field(default=None,primary_key=True)
+    qst_descricao: str = Field(index=False)
+    qst_dificuldade: str = Field(index=False)
+    qst_assunto: str = Field(index=False)
+    area_conhecimento_id: int = Field(foreign_key="area_conhecimento.acc_id")
     area_conhecimento: "Area_Conhecimento" = Relationship(back_populates="questoes")
