0d0ea63cf93f391d872b19088b78ed60518b6be9
Marcella Feliciano <153116960+MarcellaFeliciano@users.noreply.github.com>
2025-10-12 15:21:03 -0300

    update

--- frontend/projeto_integrador_frontend/src/App.jsx
+++ frontend/projeto_integrador_frontend/src/App.jsx
@@ -5,7 +5,7 @@ import "./App.css"
 
 export default function App() {
   return (
-<<<<<<< Updated upstream
+
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Inicio />} />
@@ -13,12 +13,5 @@ export default function App() {
       </Routes>
     </BrowserRouter>
   );
-=======
-    <>
 
-      {/* <Index/> */}
-      <Inicio/>
-    </>
-  )
->>>>>>> Stashed changes
 }
