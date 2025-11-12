278baf6114021d9928afea6c5eb8d6d6b80920a0
késsia <161626563+Ykessia@users.noreply.github.com>
2025-11-12 16:24:27 -0300

    ajustes

--- frontend/projeto_integrador_frontend/src/pages/Quiz.jsx
+++ frontend/projeto_integrador_frontend/src/pages/Quiz.jsx
@@ -1,13 +1,89 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Inicio.css";
+import logo from '../assets/logo_XPENEM.png';
+import icon_alvo from '../assets/icon_alvo.png';
+import icon_cronometro from '../assets/icon_cronometro.png';
+import icon_grafico from '../assets/icon_grafico.png';
+import icon_trofeu from '../assets/icon_trofeu.png';
+import Login from "./Login";
 
 export default function Quiz() {
+  const [mostrarLogin, setMostrarLogin] = useState(false);
 
-return (
+  return (
     <>
-    <h1>teste quiz</h1>
-    <Link to="/" className="button-menu">Inicio</Link>
+      <div className="main_page">
+        <header>
+          <div className="img-logo">
+            <img src={logo} alt="logo" />
+          </div>
+          <nav>
+            <Link to="/" className="button-menu">Inicio</Link>
+            <Link to="/quiz" className="button-menu">Quiz</Link>
+            <Link to="/sobre" className="button-menu">Sobre</Link>
+            <button onClick={() => setMostrarLogin(true)}>Login</button>
+
+            <Link to="/perfil" className="icon-perfil">icon</Link>
+          </nav>
+        </header>
+        <section>
+          {/* <div className="intro">
+            <div className="div_info_intro">
+              <h1>Domine o Enem com confiança!</h1>
+              <p>Aprenda no seu ritmo e conquiste o seu futuro.</p>
+              <button>Começar agora</button>
+            </div>
+          </div> */}
+
+          <div className="info">
+            <div>
+              <div className="info-titulo">
+                <span><img src={icon_alvo} alt="" /></span>
+                <h2>Questões no estilo enem</h2>
+              </div>
+              <div className="info-detalhe">
+                <p>Simule a prova real com perguntas de provas passadas.</p>
+              </div>
+            </div>
+            <div>
+              <div className="info-titulo">
+                <span><img src={icon_cronometro} alt="" /></span>
+                <h2>Estude no seu ritmo</h2>
+              </div>
+              <div className="info-detalhe">
+                <p>Organize seu tempo e estude conforme sua rotina.</p>
+              </div>
+            </div>
+            <div>
+              <div className="info-titulo">
+                <span><img src={icon_grafico} alt="" /></span>
+                <h2>Veja o seu desempenho</h2>
+              </div>
+              <div className="info-detalhe">
+                <p>Acompanhe a evolução e melhore suas áreas deficientes.</p>
+              </div>
+            </div>
+            <div>
+              <div className="info-titulo">
+                <span><img src={icon_trofeu} alt="" /></span>
+                <h2>Ganhe recompensas</h2>
+              </div>
+              <div className="info-detalhe">
+                <p>Alcance suas metas e conquiste troféus.</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="description"></div>
+        </section>
+      </div>
+
+        {/* MOSTRAR O MODAL */}
+        {mostrarLogin && (
+            <Login fecharModal={() => setMostrarLogin(false)} />
+        )}
+
     </>
-)
+  );
 }
--- frontend/projeto_integrador_frontend/src/pages/Sobre.jsx
+++ frontend/projeto_integrador_frontend/src/pages/Sobre.jsx
@@ -1,13 +1,55 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import logo from '../assets/logo_XPENEM.png';
 import "./Inicio.css";
 
 export default function Sobre() {
 
 return (
     <>
-    <h1>teste sobre</h1>
-    <Link to="/" className="button-menu">Inicio</Link>
+    <div className="main_page">
+        <header>
+            <div className="img-logo">
+                <img src={logo} alt="logo" />
+            </div>
+            <nav>
+                <Link to="/" className="button-menu">Inicio</Link>
+                <Link to="/quiz" className="button-menu">Quiz</Link>
+                <Link to="/sobre" className="button-menu">Sobre</Link>
+                <button onClick={() => setMostrarLogin(true)}>Login</button>
+
+                <Link to="/perfil" className="icon-perfil">icon</Link>
+            </nav>
+        </header>
+        <section>
+            <h2>🧭 Sobre Nós: Bem-vindo ao XPEnem, uma plataforma criada para transformar a forma como os estudantes se preparam para o ENEM e vestibulares! 🚀 Nosso objetivo é tornar o aprendizado mais leve, eficiente e personalizado, ajudando cada aluno a entender seus pontos fortes e onde pode melhorar. Desde 2023, reunimos uma equipe apaixonada por educação, tecnologia e design para criar ferramentas interativas, simulados inteligentes e dashboards de desempenho que ajudam você a evoluir de verdade.</h2>
+            <h3>💡 O Que Fazemos
+
+📊 Painéis Inteligentes: Acompanhe seu progresso com gráficos dinâmicos e relatórios detalhados.
+
+🧠 Simulados Personalizados: Gere provas adaptadas ao seu nível e área de interesse.
+
+🎯 Sistema de Recompensas: Ganhe pontos e badges ao alcançar metas e desafios semanais.
+
+🤝 Comunidade de Estudo: Participe de grupos e troque experiências com outros alunos.
+
+🌟 Nossa Missão
+
+Democratizar o acesso a uma educação de qualidade, combinando dados, tecnologia e empatia para impulsionar o potencial de cada estudante.
+
+👥 Nossa Equipe
+
+Somos um grupo diverso de professores, desenvolvedores e designers que acreditam que aprender pode (e deve!) ser divertido.
+
+📍 Curiosidades Aleatórias
+
+Nosso mascote se chama Pixel, e ele nasceu de um erro de código que virou piada interna. 😅
+
+Já ajudamos mais de 50 mil estudantes a simularem suas notas no ENEM.
+
+Temos uma playlist no Spotify chamada “Foco e Café” feita especialmente para quem estuda à noite. ☕🎶</h3>
+        </section>
+    </div>
     </>
 )
 }
